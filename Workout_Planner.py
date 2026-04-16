1a9eff94181a337deca865d39963473682b9ec63
Danielius <danieliuscudinas06@gmail.com>
2026-04-16 14:51:24 +0300

    Sukurtas ProgressTracker klase ir parašyta logiką ieškoti didžiausia svorį

--- Workout_Planner.py
+++ Workout_Planner.py
@@ -8,6 +8,7 @@ class Exersice:
 
 
 
+
 class StrengthExercise(Exersice):
     def __init__(self, name, muscle):
         super().__init__(name)
@@ -43,3 +44,42 @@ class WorkoutPlan:
         print(f"\nPlanas: {self.name}")
         for ex in self.exercises:
             print("-", ex.get_info())
+
+
+
+
+class Set:
+    def __init__(self, exercise, reps, weight):
+        self.exercise = exercise
+        self.reps = reps
+        self.weight = weight
+
+    def get_info(self):
+        return f"{self.exercise.name}: {self.reps} reps x {self.weight} kg"
+
+
+class WorkoutSession:
+    def __init__(self, date):
+        self.date = date
+        self.sets = []
+
+    def add_set(self, set_obj):
+        self.sets.append(set_obj)
+
+    def show(self):
+        print(f"\nTreniruotė: {self.date}")
+        for s in self.sets:
+            print("-", s.get_info())
+
+
+
+ex1 = StrengthExercise("Bench Press", "Chest")
+ex2 = CardioExercise("Running", 30)
+
+workout = WorkoutSession("2026-04-16")
+
+session = WorkoutPlan("Kruitne ir Kardio")
+session.add_exercise(ex1)
+session.add_exercise(ex2)
+
+session.show()
